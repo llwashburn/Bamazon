cd6559fcdabb59e8b4a0ec7a5c2bfd759c80fc70
llwashburn <llwashburn@gmail.com>
2018-03-02 13:28:23 -0500

    final push

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,7 @@
 //create data connection
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+const cTable = require('console.table');
 
 // create connection object but using mysql package and function
 // pass in connection credentials
@@ -13,164 +14,126 @@ var connection = mysql.createConnection({
 });
 
 
-// connect to database-open connection
-//connection -name of var from above
-//.method is .connect which will execute a connection using credentials 
+// connect to database-open connection \ method is .connect which will execute a connection using credentials 
 //callback function to know what to do after connects
 connection.connect(function(err) {
     if (err) throw err;
     console.log("Connected " + connection.threadId);
     // thread Id is a property of our connection
-    // connection.end();
-    // close connection
-
-    //start function
-    start();
+    // connection.end();close connection
+    start(); //start function
 
 });
 //start function 
 //prompt user if they would like to purchase a product.
 function start() {
     inquirer.prompt({
-        name: "purchaseProduct",
-        type: "list", //or input?
-        message: "Would you like to purchase a product today?",
-        choices: ["YES", "NO"]
-            //use validate with inquirer module so that the user input is what you expect
-            //validate: function(value){
-            //if (isNan(value) === false) {
-            //return true;
-            //         }
-            //         return false:
-            //     }
-    })
+            name: "purchaseProduct",
+            type: "list", //or input?
+            message: "Welcome to Bamazon! Would you like to purchase a product today?",
+            choices: ["YES", "NO"]
 
-    //if yes then go to next step
-    //if no then console log a message
-    .then(function(answer) {
-        //based on their answer, either display products or console log another message.
-        //return the user's response to UPPERCASE so that it matches conditional
-        if (answer.purchaseProduct.toUpperCase() === "YES") {
-            displayProducts();
-        } else {
-            console.log("Please visit our site again. Have a nice day")
-            connection.end();
-            //close connection - end of conversation
-        }
-    });
+        })
+        //if yes then go to next step
+        .then(function(answer) {
+            //based on their answer, either display products or console log another message.
+            //return the user's response to UPPERCASE so that it matches conditional
+            if (answer.purchaseProduct.toUpperCase() === "YES") {
+                displayProducts();
+            } else {
+                console.log("Please visit our site again. Have a nice day")
+                    //if no then console log a message
+                connection.end();
+                //close connection - end of conversation
+            }
+        });
 }
 
-//display products for sale if user chooses yes
 function displayProducts() {
     //query the database for all the items in the bamazon database
     connection.query("SELECT * FROM products", function(err, results) {
         if (err) throw err;
-        //once you have the items, prompt the user for which they'd like to bid on
-        connection.connect();
-        //connect to database to get information and put in a table
-        //create the table
-        var table = productTable
-            //item ID, product name, price
-            //for loop - loop through the items and display the data found in the loop in table format
-            //check to see if the quanitity is available  
-        for (var i = 0; i < results.length; i++) {
-            //productTable.push(results[i].item_id, product_name, department_name, price, stock_quantity);
-            console.log(results)
-            console.log("item_id", "product_name", "department_name ", "price", "stock_quantity ")
-
-
-            .prompt([{
+        //prompt user to as what item they would like to purchase from the database
+        //BEFORE INQUIRER DISPLAY TABLE OF PRODUCTS
+        console.table(results)
+        inquirer.prompt([{
                 name: "choice",
                 type: "list",
+                message: "What item would you like to purchase? Use the arrow key to make your selection.",
                 //choices: inquirer list all options
+                //INSTEAD OF THESE STRINGS, DISPLAY JUST results[i].item_id
                 choices: function() {
                     var choiceArray = [];
                     for (var i = 0; i < results.length; i++) {
-                        choiceArray.push(results[i].item_id, product_name, department_name, price, stock_quantity);
-                    }
-                    return choiceArray;
-                },
-                message: "What item would you like to purchase?"
+                        choiceArray.push(results[i].item_id.toString());
+                    };
+                    return choiceArray; //return all the user choices
+                    //how to create the quantity function to run next? 
+                }
+
             }])
-        }
-    });
+            .then(function(response) {
+                productQuantity(results, response.choice);
+            })
+    })
+};
 
+//results = sql query results, and choice = user chosen item_id
+function productQuantity(results, choice) {
+    //make connection again - look at data and then ask user questions
 
+    inquirer.prompt([{
+            name: "quantity",
+            //prompt user to enter a number for quantity
+            type: "input",
+            //ask user how many products they would like to purchase. 
+            message: "How many products would you like to purchase?",
+        }])
+        .then(function(response) {
+            //create variable to hold the results?
+            var product;
+            for (var i = 0; i < results.length; i++) {
+                if (results[i].item_id === parseInt(choice)) {
+                    product = results[i];
+                }
+            }
+            //console.log(product)
+            //determine if data results match user quantity
+            if (product.stock_quantity >= parseInt(response.quantity)) //if this is true then change quantity in database
+                console.log("This product has been added to your order"),
+                updateInventory(choice, parseInt(response.quantity));
+            else {
+                console.log("This product is not available");
+                //restart?
+                start();
+            }
+        })
 
-    //             .then(function(answer) {
-    //                     var chosenItem;
-    //                     for (var i = 0; i < results.length; i++) {
-    //                         if (results[i].product_name === answer.choice) {
-    //                             chosenItem = results[i];
-    //                         }
-    //                     }
+}
 
-    //                     if (chosenItem.stock_quantity < parseInt(answer.stock_quantity)) {
-    //                         connection.query(
-    //                             "UPDATE ? SET ? WHERE ?",
-    //                         ]
-    //                     )
-    //                 }
+function updateInventory(choice, quantity) {
+    console.log('update here')
+        //query the database for the stock_quantity  - use select or update? 
+        //UPDATE database to reduce stock_quantity for the given item_id (choice)
+    connection.query("UPDATE products SET stock_quantity = stock_quantity - ? WHERE item_id = ?", [quantity, choice], function(err, data) {
+            if (err) throw err;
+            console.log(data.affectedRows + ' item(s) successfully updated!');
+            start();
+        })
+        // if (err) throw err;
+}
+//then compare the response to the current inventory
+//create a variable to store new inventory? 
+// var updatedQuantity = results[i].stock_quantity
+//     //somehow subtract the response from the current inventory
+// updatedQuantity - choice
+//     //update the mysql database 
+// connection.query("UPDATE stock_quantity FROM products WHERE ?")
+// if (err) throw err;
 
-    //                 //connection.connect(); 
+// // do I need to set
+// [{
+//     stock_quantity: results.stock_quantity,
+// }]
 
-    //                 //connect to database to get information and put in a table
-
-    //                 //create the table
-    //                 var table = productTable
-    //                     //item ID, product name, price
-
-    //                 //for loop - loop through the items and display the data found in the loop in table format
-    //                 //check to see if the quanitity is available 
-    //                 //if not console.log message stating that this item is not avaiable in this quanitity. 
-
-    //                 //else if the store has enough of the product then the customer order can be filled.
-    //                 //console.log 
-
-    //                 //create a function to buy/purchase
-
-    //                 //call the function
-    //                 //update mySQl database to reflect the remaining quantity. 
-    //                 //once order goes through show the customer the total cost of their purchase.
-
-
-
-
-
-
-
-
-
-
-
-    //                 inquirer.prompt([{
-    //                     name: "item",
-    //                     type: "input",
-    //                     message: "What items would you like to purchase today? Please enter product ID number.",
-    //                     //validate property to make sure the user's response is the correct data type
-    //                     validate: function(value) {
-    //                         if (isNaN(value) === false) {
-    //                             return true;
-    //                         }
-    //                         return false;
-    //                     }
-
-    //                 }])
-    //                 //ask user how many items they would like to purchase
-    //                 inquirer.prompt([{
-    //                     name: "item",
-    //                     type: "input",
-    //                     message: "How many items would you like to purchase?",
-    //                     //validate property to make sure the user's response is the correct data type
-    //                     validate: function(value) {
-    //                         if (isNaN(value) === false) {
-    //                             return true;
-    //                         }
-    //                         return false;
-    //                     }
-
-    //                 }])
-    //             })
-    //     };
-    // }
-    //
+// connection.query("UPDATE stock_quantity FROM products WHERE ?")
